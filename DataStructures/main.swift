41a65050e6d4ba10f2186f85230a631e4dab74e6
Bruno Alves <alves.bruno945@gmail.com>
2024-08-27 21:54:40 -0300

    Adding `pop` method which is responsable to remove the top item from stack

--- DataStructures/main.swift
+++ DataStructures/main.swift
@@ -8,8 +8,18 @@ func push(_ element: String) {
     stack.append(element)
 }
 
+func pop() -> String {
+    let popTopItem = stack.popLast()
+    return popTopItem ?? "Something went wrong"
+}
+
 push("Kingdom Hearts")
 push("Dark Souls")
 push("Final Fantasy")
 
 print("Initial Values: \(stack)")
+
+let topItem = pop()
+
+print("Removed Value: \(topItem)")
+print("Values after `pop` method: \(stack)")
