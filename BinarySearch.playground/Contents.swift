03a14870771629d721a27d680dd934fa58011e24
Bruno Alves <alves.bruno945@gmail.com>
2024-09-10 23:08:13 -0300

    Implementing binarySearch

--- BinarySearch.playground/Contents.swift
+++ BinarySearch.playground/Contents.swift
@@ -1,7 +1,25 @@
 import Foundation
 
-func binarySearch(list: [Int], item: Int) {
-    
+func binarySearch(list: [Int], item: Int) -> Int? {
+    var low = 0
+    var high = list.count - 1
+
+    while low <= high {
+        var middle = low + high / 2
+        var attempt = list[middle]
+
+        if attempt == item {
+            return middle
+        }
+
+        if attempt > item {
+            high = middle - 1
+        } else {
+            low = middle + 1
+        }
+    }
+
+    return nil
 }
 
 var orderedList = [1, 3, 5, 7, 9]
