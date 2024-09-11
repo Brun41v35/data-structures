43d34b84fc2827834512aeeee9505d5c3bd32c31
Bruno Alves <alves.bruno945@gmail.com>
2024-09-11 13:17:10 -0300

    Fix binarySearch method

--- BinarySearch.playground/Contents.swift
+++ BinarySearch.playground/Contents.swift
@@ -1,27 +1,27 @@
 import Foundation
 
-func binarySearch(list: [Int], item: Int) -> Int? {
+func binarySearch(list: [Int], item: Int) -> Int {
     var low = 0
     var high = list.count - 1
+    var middle = 0
+    var attempt = 0
 
     while low <= high {
-        var middle = low + high / 2
-        var attempt = list[middle]
+        middle = low + (high - low) / 2
+        attempt = list[middle]
 
         if attempt == item {
             return middle
-        }
-
-        if attempt > item {
+        } else if attempt > item {
             high = middle - 1
         } else {
             low = middle + 1
         }
     }
 
-    return nil
+    return -1
 }
 
 var orderedList = [1, 3, 5, 7, 9]
 
-binarySearch(list: orderedList, item: 3)
+print(binarySearch(list: orderedList, item: 10))
